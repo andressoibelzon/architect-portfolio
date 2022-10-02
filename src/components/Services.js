5ef6337742a3cab1f765a31f4dcb8430657af045
andressoibelzon <a.soibelzon@gmail.com>
2022-10-02 18:33:34 +0200

    second commit

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,6 +1,16 @@
 import '../Services.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import image1 from "../assets/img/image1.jpg"
+import image1 from "../assets/img/image1.jpg";
+import image2 from "../assets/img/image2.jpg";
+import image3 from "../assets/img/image3.jpg";
+import image4 from "../assets/img/image4.jpg";
+import image5 from "../assets/img/image5.jpg";
+import image6 from "../assets/img/image6.jpg";
+import image7 from "../assets/img/image7.jpg";
+import image8 from "../assets/img/image8.jpg";
+import image9 from "../assets/img/image9.jpg";
+
+
 
 
 export const Services = () => {
@@ -14,72 +24,72 @@ export const Services = () => {
                         <div class="profile-card-2">
                             <img src={image1} alt="image1" class="img img-responsive"></ img>
                             <div class="profile-name">Renders</div>
-                            <div class="profile-username">Este es una descripcion de lo que se realiza aqui adentro, luego se ingresa a otra seccion.</div>
+                            <div class="profile-username">Imagen fotorrealista exterior o interior</div>
                             <div class="profile-icons"><a href="#"><i class="fa fa-facebook"></i></a><a href="#"><i class="fa fa-twitter"></i></a><a href="#"><i class="fa fa-linkedin"></i></a></div>
                         </div>
                     </div>
                     <div class="col-md-4">
                         <div class="profile-card-2">
-                        <img src={image1} alt="image1" class="img img-responsive"></ img>
+                        <img src={image2} alt="image2" class="img img-responsive"></ img>
                             <div class="profile-name">Videos</div>
-                            <div class="profile-username">Este es una descripcion de lo que se realiza aqui adentro, luego se ingresa a otra seccion.</div>
+                            <div class="profile-username">Video arquitectonico recorrible para visualizar el proyecto</div>
                             <div class="profile-icons"><a href="#"><i class="fa fa-facebook"></i></a><a href="#"><i class="fa fa-twitter"></i></a><a href="#"><i class="fa fa-linkedin"></i></a></div>
                         </div>
                     </div>
                     <div class="col-md-4">
                         <div class="profile-card-2">
-                        <img src={image1} alt="image1" class="img img-responsive"></ img>
+                        <img src={image3} alt="image1" class="img img-responsive"></ img>
                             <div class="profile-name">Brochures</div>
-                            <div class="profile-username">Este es una descripcion de lo que se realiza aqui adentro, luego se ingresa a otra seccion.</div>
+                            <div class="profile-username">Elemento descriptivo simple para entender todo el proyecto, con textos, imagenes y graficos</div>
                             <div class="profile-icons"><a href="#"><i class="fa fa-facebook"></i></a><a href="#"><i class="fa fa-twitter"></i></a><a href="#"><i class="fa fa-linkedin"></i></a></div>
                         </div>
                         
                     </div>
                     <div class="col-md-4">
                         <div class="profile-card-2">
-                        <img src={image1} alt="image1" class="img img-responsive"></ img>
+                        <img src={image4} alt="image1" class="img img-responsive"></ img>
                             <div class="profile-name">Recorrido Virtual</div>
-                            <div class="profile-username">Este es una descripcion de lo que se realiza aqui adentro, luego se ingresa a otra seccion.</div>
+                            <div class="profile-username">Para poder recorrrer como un juego el proyecto</div>
                             <div class="profile-icons"><a href="#"><i class="fa fa-facebook"></i></a><a href="#"><i class="fa fa-twitter"></i></a><a href="#"><i class="fa fa-linkedin"></i></a></div>
                         </div>
                     </div>
                     <div class="col-md-4">
                         <div class="profile-card-2">
-                        <img src={image1} alt="image1" class="img img-responsive"></ img>
+                        <img src={image5} alt="image1" class="img img-responsive"></ img>
                             <div class="profile-name">Pagina Web y Marketing</div>
-                            <div class="profile-username">Este es una descripcion de lo que se realiza aqui adentro, luego se ingresa a otra seccion.</div>
+                            <div class="profile-username">Diseñamos tu pagina web y utilizamos herramientas de SEO para el posicionamiento</div>
                             <div class="profile-icons"><a href="#"><i class="fa fa-facebook"></i></a><a href="#"><i class="fa fa-twitter"></i></a><a href="#"><i class="fa fa-linkedin"></i></a></div>
                         </div>
                     </div>
                     <div class="col-md-4">
                         <div class="profile-card-2">
-                        <img src={image1} alt="image1" class="img img-responsive"></ img>
+                        <img src={image6} alt="image1" class="img img-responsive"></ img>
                             <div class="profile-name">Plantas Pintadas</div>
-                            <div class="profile-username">Este es una descripcion de lo que se realiza aqui adentro, luego se ingresa a otra seccion.</div>
+                            <div class="profile-username">Plantas de arquitectura decoradas para que se vean mejor</div>
                             <div class="profile-icons"><a href="#"><i class="fa fa-facebook"></i></a><a href="#"><i class="fa fa-twitter"></i></a><a href="#"><i class="fa fa-linkedin"></i></a></div>
                         </div>
                     </div>
                     <div class="col-md-4">
                         <div class="profile-card-2">
-                        <img src={image1} alt="image1" class="img img-responsive"></ img>
+                        <img src={image7} alt="image1" class="img img-responsive"></ img>
                             <div class="profile-name">Axonometricas</div>
-                            <div class="profile-username">Este es una descripcion de lo que se realiza aqui adentro, luego se ingresa a otra seccion.</div>
+                            <div class="profile-username">Mediante las axonometricas, se entienda de una forma clara, todo el proyecto</div>
                             <div class="profile-icons"><a href="#"><i class="fa fa-facebook"></i></a><a href="#"><i class="fa fa-twitter"></i></a><a href="#"><i class="fa fa-linkedin"></i></a></div>
                         </div>
                     </div>
                     <div class="col-md-4">
                         <div class="profile-card-2">
-                        <img src={image1} alt="image1" class="img img-responsive"></ img>
+                        <img src={image8} alt="image1" class="img img-responsive"></ img>
                             <div class="profile-name">Video Promocional</div>
-                            <div class="profile-username">Este es una descripcion de lo que se realiza aqui adentro, luego se ingresa a otra seccion.</div>
+                            <div class="profile-username">Video Marketing que agrega elementos del proyecto y footage que ayuda a la promocion del estudio</div>
                             <div class="profile-icons"><a href="#"><i class="fa fa-facebook"></i></a><a href="#"><i class="fa fa-twitter"></i></a><a href="#"><i class="fa fa-linkedin"></i></a></div>
                         </div>
                     </div>
                     <div class="col-md-4">
                         <div class="profile-card-2">
-                        <img src={image1} alt="image1" class="img img-responsive"></ img>
+                        <img src={image9} alt="image1" class="img img-responsive"></ img>
                             <div class="profile-name">Realidad Virtual</div>
-                            <div class="profile-username">Este es una descripcion de lo que se realiza aqui adentro, luego se ingresa a otra seccion.</div>
+                            <div class="profile-username">Recorriendo el proyecto mediante anteojos de inmersion</div>
                             <div class="profile-icons"><a href="#"><i class="fa fa-facebook"></i></a><a href="#"><i class="fa fa-twitter"></i></a><a href="#"><i class="fa fa-linkedin"></i></a></div>
                         </div>
                     </div>
